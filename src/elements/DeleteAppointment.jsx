d81cb7e2d90263bc9043cfcc55aa41a700d62f6e
Kuzmin Anton <akuzmin.exceedteam@gmail.com>
2021-10-15 13:23:13 +0300

    FE card-5 (edit appointment) comments

---
+++ src/elements/DeleteAppointment.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  Backdrop, Box, Button, Fade, Modal,
+} from '@mui/material';
+
+const DeleteAppointment = (props) => {
+  const {
+    handleOpenModal,
+    handleCloseModal,
+    openModal,
+  } = props;
+
+  return (
+    <div>
+      <Button onClick={handleOpenModal}>
+        <img src="../images/icon-delete.svg" alt="img" />
+      </Button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={openModal}
+        onClose={handleCloseModal}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 200,
+        }}
+      >
+        <Fade in={openModal}>
+          <Box className="modal-ed" />
+        </Fade>
+      </Modal>
+    </div>
+  );
+};
+
+export default DeleteAppointment;
--- src/elements/ModalWindow.jsx
+++ src/elements/ModalWindow.jsx
@@ -10,6 +10,8 @@ import {
 } from '@mui/material';
 import axios from 'axios';
 import './ModalWindows.scss';
+import EditAppointment from './EditAppointment';
+import DeleteAppointment from './DeleteAppointment';
 
 const ModalWindow = (props) => {
   const {
@@ -66,8 +68,9 @@ const ModalWindow = (props) => {
         authorization: token,
       },
     }).then((res) => {
-      if (!res.data.error) {
-        setAppointmentList(res.data.data);
+      const { error, data } = res.data;
+      if (!error) {
+        setAppointmentList(data);
       } else {
         logout();
       }
@@ -75,133 +78,30 @@ const ModalWindow = (props) => {
     handleCloseModal();
   };
 
-  if (typeModal === 'edit') {
-    return (
-      <div>
-        <Button onClick={handleOpenModal}>
-          <img src="../images/icon-edit.svg" alt="img" />
-        </Button>
-        <Modal
-          aria-labelledby="transition-modal-title"
-          aria-describedby="transition-modal-description"
-          open={openModal}
-          onClose={handleCloseModal}
-          closeAfterTransition
-          BackdropComponent={Backdrop}
-          BackdropProps={{
-            timeout: 200,
-          }}
-        >
-          <Fade in={openModal}>
-            <Box className="modal-ed">
-              <div className="modal-edit">
-                <div className="modal-edit-header">
-                  <h3>Изменить прием</h3>
-                </div>
-                <FormControl>
-                  <div className="modal-edit-form">
-                    <div className="modal-edit-form__input-elem">
-                      <p>Имя:</p>
-                      <TextField
-                        value={inputValueEdit.name}
-                        onChange={(e) => setInputValueEdit({
-                          ...inputValueEdit,
-                          name: e.target.value,
-                        })}
-                      />
-                    </div>
-                    <div className="modal-edit-form__input-elem">
-                      <Select
-                        name="doctor"
-                        className="main-top-inputs-fields doctor-list"
-                        value={inputValueEdit.doctor}
-                        onChange={(e) => setInputValueEdit({
-                          ...inputValueEdit,
-                          doctor: e.target.value,
-                        })}
-                      >
-                        {doctorsList.map((value, index) => (
-                          <MenuItem key={index} value={value.doctor}>{value.doctor}</MenuItem>
-                        ))}
-                      </Select>
-                    </div>
-                    <div className="modal-edit-form__input-elem">
-                      <p>Дата:</p>
-                      <TextField
-                        type="date"
-                        value={inputValueEdit.date}
-                        onChange={(e) => setInputValueEdit({
-                          ...inputValueEdit,
-                          date: e.target.value,
-                        })}
-                      />
-                    </div>
-                    <div className="modal-edit-form__input-elem">
-                      <p>Жалобы:</p>
-                      <TextField
-                        value={inputValueEdit.complaints}
-                        onChange={(e) => setInputValueEdit({
-                          ...inputValueEdit,
-                          complaints: e.target.value,
-                        })}
-                      />
-                    </div>
-                  </div>
-                </FormControl>
-                <div className="modal-edit-btns">
-                  <Button onClick={() => handleCloseModal()}>
-                    Cancel
-                  </Button>
-                  <Button onClick={() => editAppointment()}>
-                    Save
-                  </Button>
-                </div>
-              </div>
-            </Box>
-          </Fade>
-        </Modal>
-      </div>
-    );
-  }
-
   return (
-    <div>
-      <Button onClick={handleOpenModal}>
-        <img src="../images/icon-delete.svg" alt="img" />
-      </Button>
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        open={openModal}
-        onClose={handleCloseModal}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 200,
-        }}
-      >
-        <Fade in={openModal}>
-          <Box className="modal-ed">
-            {/*<div className="modal-del">*/}
-            {/*  <div className="modal-del-title">*/}
-            {/*    <h3>Удалить прием</h3>*/}
-            {/*  </div>*/}
-            {/*  <div className="modal-del-text">*/}
-            {/*    <p>Вы действительно хотите удалить прием?</p>*/}
-            {/*  </div>*/}
-            {/*  <div className="modal-edit-btns">*/}
-            {/*    <Button onClick={() => handleCloseModal()}>*/}
-            {/*      Cancel*/}
-            {/*    </Button>*/}
-            {/*    <Button>*/}
-            {/*      Delete*/}
-            {/*    </Button>*/}
-            {/*  </div>*/}
-            {/*</div>*/}
-          </Box>
-        </Fade>
-      </Modal>
-    </div>
+    <>
+      {
+        typeModal === 'edit'
+          ? (
+            <EditAppointment
+              doctorsList={doctorsList}
+              openModal={openModal}
+              handleOpenModal={handleOpenModal}
+              handleCloseModal={handleCloseModal}
+              inputValueEdit={inputValueEdit}
+              setInputValueEdit={setInputValueEdit}
+              editAppointment={editAppointment}
+            />
+          )
+          : (
+            <DeleteAppointment
+              handleOpenModal={handleOpenModal}
+              handleCloseModal={handleCloseModal}
+              openModal={openModal}
+            />
+          )
+      }
+    </>
   );
 };
 
